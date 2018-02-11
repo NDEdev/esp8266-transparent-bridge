60f070eaf5fe4a3902488741a00e81fc5621b1b7
dNdt <demidnau@gmail.com>
2018-02-11 22:37:44 +0700

    Initial commit. Coneccting to wi-fi network

---
+++ esp.ino
@@ -0,0 +1,29 @@
+#include "ESP8266WiFi.h"
+
+// WiFi parameters to be configured
+const char* ssid = "Asusnet";
+const char* password = "AoDjCEyu";
+
+void setup(void)
+{
+  Serial.begin(115200);
+  // Connect to WiFi
+  Serial.print("Connecting to wifi: Asusnet");
+  WiFi.begin(ssid, password);
+
+  // while wifi not connected yet, print '.'
+  // then after it connected, get out of the loop
+  while (WiFi.status() != WL_CONNECTED) {
+     delay(500);
+     Serial.print(".");
+  }
+  //print a new line, then print WiFi connected and the IP address
+  Serial.println("");
+  Serial.println("WiFi connected");
+  // Print the IP address
+  Serial.println(WiFi.localIP());
+
+}
+void loop() {
+  // Nothing
+}
---
+++ sloeber.ino.cpp
@@ -0,0 +1,16 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2018-02-11 22:35:49
+
+#include "Arduino.h"
+#include "ESP8266WiFi.h"
+
+void setup(void) ;
+void loop() ;
+
+#include "esp.ino"
+
+
+#endif
